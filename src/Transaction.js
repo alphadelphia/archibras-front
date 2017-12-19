c751fea0a4f495098312aa96065b72668da5c062
alphadelphia <phalansterie@gmail.com>
2017-12-19 16:40:16 +0200

    Transaction link to a popup (needs content and styles)

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,14 +1,39 @@
 import React from 'react'; 
 
+//popup code https://codepen.io/bastianalbers/pen/PWBYvz
+
+const Popup = (props) => {
+	 return(
+      <div className='popup' >
+        <div className='popup_inner'>
+          <h1>{props.text}</h1>
+        </div>
+      </div>
+    );
+};
+
 class Transaction extends React.Component {
 
 	constructor(props) {
 	    super(props);
 
+	    this.state = {
+	      showPopup: false
+	    };
 	    var iconCategory = null;
 	    //var iconAsset = null;
 
 	    this.setIcon(this.props.data.category);
+  	}
+
+  	togglePopup = () => {
+  		this.setState({
+	      showPopup: !this.state.showPopup
+	    });
+  	}
+
+  	handleClick = () => {
+  		this.togglePopup();
   	}
 
 	setIcon = (category) => {
@@ -38,16 +63,23 @@ class Transaction extends React.Component {
 	render() {
 		let date = new Date(this.props.data.time * 1000);
 		let time = date.toString();
-		
+
+		let popup = this.state.showPopup ? 
+			 <Popup text="Close Me"/> : "";
+
+
+		//let popup = this.state.showPopup ? "POPUP" : "";
+
 		return(
 			<li>
-			<div>
+			<div onClick={this.handleClick}>
 				<span className = "tx-icon-left">{this.iconCategory}</span>
 					<div className = "tx-content">
 						<h2 className = "tx-main">{this.props.data.amount}</h2>
 						<h3 className = "tx-sub">{time}</h3>
 					</div>
 			</div>		
+			{popup}
 			</li>
 
 		);		
@@ -55,3 +87,5 @@ class Transaction extends React.Component {
 
 }
 export default Transaction;
+
+// <button onClick={this.togglePopup.bind(this)}>show popup</button>
